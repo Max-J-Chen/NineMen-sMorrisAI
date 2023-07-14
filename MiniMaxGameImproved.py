daf8adec101d6fef1d8f81dc45d64214ddf7e2d5
Max-J-Chen <maxchen828@gmail.com>
2023-07-13 23:38:26 -0500

    Fixed one system argument

--- MiniMaxGameImproved.py
+++ MiniMaxGameImproved.py
@@ -2,8 +2,8 @@ import minimax
 import sys
 import helper
 
-# max_depth = int(sys.argv[1])
-max_depth = 3
+max_depth = int(sys.argv[1])
+
 minimax.minimax(max_depth=max_depth,
                 phase=2,
                 static_estimate=helper.static_estimation_mid_improved,
